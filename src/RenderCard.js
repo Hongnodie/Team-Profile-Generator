d282a99f4ff262440272724587f12504a71d521a
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-02-28 23:32:36 +1030

    render function completed

--- src/RenderCard.js
+++ src/RenderCard.js
@@ -1,6 +1,36 @@
-function render() {
-    
-}
+const path = require("path");
+const fs = require("fs");
 
+const templatesDir = path.resolve(__dirname, "../dist");
+
+const render = employees => {
+  const html = [];
+
+  html.push(...employees.map(employee => renderEmployee(employee))
+  );
+
+  return renderMain(html.join(""));
+
+};
+
+const renderEmployee = employee => {
+  let template = fs.readFileSync(path.resolve(templatesDir, `${employee.role}.html`), "utf8");
+  template = replacePlaceholders(template, "name", employee.name);
+  template = replacePlaceholders(template, "role", employee.role);
+  template = replacePlaceholders(template, "email", employee.email);
+  template = replacePlaceholders(template, "id", employee.id);
+  template = replacePlaceholders(template, "special", employee.special);
+  return template;
+};
+
+const renderMain = html => {
+    const template = fs.readFileSync(path.resolve(templatesDir, "index.html"), "utf8");
+    return replacePlaceholders(template, "team", html);
+};
+
+const replacePlaceholders = (template, placeholder, value) => {
+    const holder = new RegExp("{{ " + placeholder + " }}", "gm");
+    return template.replace(holder, value);
+};
 
 module.exports = render;
